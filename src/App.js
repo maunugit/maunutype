8940c2509fba3a8001b814f372bc99c43deb798b
Maunu Aunesluoma <142994994+maunugit@users.noreply.github.com>
2023-09-10 13:19:54 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,8 +13,9 @@ function shuffleArray(array) {
 
 const words = [
   'hello', 'world', 'react', 'javascript', 'typing', 'challenge',
-  'developer', 'programming', 'openai', 'computer', 'keyboard', 'speed',
-  'green', 'operator', 'complete', 'structure', 'brains', 'tree', 'bright', 'brainstorm','insight'
+  'developer', 'programming', 'database', 'computer', 'keyboard', 'speed',
+  'green', 'operator', 'complete', 'structure', 'brains', 'tree', 'bright', 'brainstorm', 'insight'
+  'probability', 'falsehood', 'cover', 'yellow', 'biomedical', 'before', 'constant'
 ];
 
 

84077dd4f602b93d05eade870f6a8b5efea42731
Maunu Aunesluoma <142994994+maunugit@users.noreply.github.com>
2023-09-10 13:57:01 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,9 @@ const words = [
   'hello', 'world', 'react', 'javascript', 'typing', 'challenge',
   'developer', 'programming', 'database', 'computer', 'keyboard', 'speed',
   'green', 'operator', 'complete', 'structure', 'brains', 'tree', 'bright', 'brainstorm', 'insight',
-  'probability', 'falsehood', 'cover', 'yellow', 'biomedical', 'before', 'constant'
+  'probability', 'falsehood', 'cover', 'yellow', 'biomedical', 'before', 'constant', 'provide', 'impossible',
+  'gradual', 'push', 'internet', 'accuracy', 'timeless', 'distribution', 'firm', 'intelligence', 'but',
+  'leftovers', 'tribal', 'mouse', 'graphic', 'processor', 'given', 'amount', 'sight'
 ];
 
 
